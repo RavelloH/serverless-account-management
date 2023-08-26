fc8dacb05385e59242936a8285ffaa4c9497ee35
RavelloH <hyh20060327@qq.com>
2023-08-26 21:37:46 +0800

    Fix. 修复异步

--- api/signup.js
+++ api/signup.js
@@ -7,7 +7,7 @@ const shuffler = require('../utils/shuffler')
 const prisma = new PrismaClient()
 
 async function signup(username, nickname, email, password) {
-    let encryptPassword = encrypt(password)
+    await let encryptPassword = encrypt(password)
     console.log('pwd', encryptPassword)
     await prisma.user.create({
         data: {
