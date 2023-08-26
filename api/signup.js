9fe65d8a2be45092d243e3afce21f6d6f4ccfa39
RavelloH <hyh20060327@qq.com>
2023-08-26 21:00:44 +0800

    Fix. 修复hash promise

--- api/signup.js
+++ api/signup.js
@@ -1,14 +1,16 @@
 const {
     PrismaClient
 } = require('@prisma/client')
-const { argon2 } = require("argon2");
+const {
+    argon2
+} = require("argon2");
 const newResponse = require('../utils/response')
 const shuffler = require('../utils/shuffler')
 const prisma = new PrismaClient()
 
 async function signup(username, nickname, email, password) {
     let encryptPassword = encrypt(password)
-    console.log('pwd',encryptPassword)
+    console.log('pwd', encryptPassword)
     await prisma.user.create({
         data: {
             username: username,
@@ -24,26 +26,31 @@ async function signup(username, nickname, email, password) {
     })
 }
 async function encrypt(password) {
-  const pwd = shuffler(password);
-  console.log('[shuffler]',pwd)
-  const options = {
-    timeCost: 4,
-    memoryCost: 655360,
-    parallelism: 1,
-    // hashLength: 32,
-  };
-  const hashedPassword = await argon2.hash(pwd,options);
-  return hashedPassword
+    const pwd = shuffler(password);
+    console.log('[shuffler]', pwd)
+    const options = {
+        timeCost: 4,
+        memoryCost: 655360,
+        parallelism: 1,
+        // hashLength: 32,
+    };
+    try {
+        const hashedPassword = await argon2.hash(pwd, options);
+    } catch (err) {
+        console.log('error:',err)
+    }
+    console.log(hashedPassword)
+    return hashedPassword
 }
 
 module.exports = (req, res) => {
     const {
         info
     } = req.query;
-    console.log('[info]',info)
+    console.log('[info]', info)
     let infoJSON = JSON.parse(info)
-    console.log('[infoJSON]',infoJSON)
-    signup(infoJSON.username,infoJSON.nickname,infoJSON.email,infoJSON.password)
+    console.log('[infoJSON]', infoJSON)
+    signup(infoJSON.username, infoJSON.nickname, infoJSON.email, infoJSON.password)
     .then(async () => {
         await prisma.$disconnect()
     })
