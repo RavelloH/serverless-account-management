1db2a4a5a012a9006e24979354824a4a73a9309f
RavelloH <hyh20060327@qq.com>
2023-09-03 12:30:44 +0800

    Feat. 响应内容优化

--- utils/response.js
+++ utils/response.js
@@ -1,16 +1,18 @@
-function newResponse(statusCode, inner, message) {
-    return {
-        statusCode: statusCode,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            'code': statusCode,
-            'message': message,
-            'inner': inner
-        })
+function newResponse(response, statusCode, message, inner) {
+    response.writeHead(statusCode, {
+        'Content-Type': 'application/json',
+        'Platform-Name': 'RPlatform'
+    })
+    
+    let returnMessage = {
+        'code': statusCode,
+        'message': message,
+        'inner': inner ? JSON.stringify(inner) : {}
     }
+    let messageJson = JSON.stringify(returnMessage,null,2)
+    
+    console.log('[Return]',statusCode,returnMessage)
+    response.end(returnMessage)
 }
 
 module.exports = newResponse;
