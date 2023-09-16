e169f65d499b0b0b13f54eb120e410421bfb1e3a
RavelloH <hyh20060327@qq.com>
2023-09-16 21:34:10 +0800

    Change. 修改数据格式

--- api/signin.js
+++ api/signin.js
@@ -22,7 +22,7 @@ async function updateTime(uid, time) {
             uid: uid
         },
         data: {
-            lastUseAt: time
+            lastUseAt: time+''
         },
     });
     console.log("[DB Writed]", timeMonitor(startTime));
