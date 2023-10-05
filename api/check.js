b8fc3bad2a76dd57d52482221fbc34700bd089c5
RavelloH <hyh20060327@qq.com>
2023-10-05 20:50:29 +0800

    Fix. 修复查询准度

--- api/check.js
+++ api/check.js
@@ -36,7 +36,7 @@ module.exports = (req, res) => {
             queryStartTime = Date.now();
             getDBInfo()
             .then((result) => {
-                newResponse(res, 200, `服务运行正常，平均查询耗时: ${Date.now() - queryStartTime}ms`, result);
+                newResponse(res, 200, `服务运行正常，平均查询耗时: ${Math.round((Date.now() - queryStartTime)/3)}ms`, result);
             })
             .catch((e)=> {
                 newResponse(res, 400, '服务无法正常运行', {
